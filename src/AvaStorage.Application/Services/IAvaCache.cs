1714fb7462cde33d7cdf9562d483726ca24570ea
Andrey O. <ozzy.ext@gmail.com>
2025-04-01 17:14:19 +0300

    Introduce Ava cache

---
+++ src/AvaStorage.Application/Services/IAvaCache.cs
@@ -0,0 +1,11 @@
+﻿using AvaStorage.Domain;
+using AvaStorage.Domain.ValueObjects;
+
+namespace AvaStorage.Application.Services
+{
+    public interface IAvaCache
+    {
+        Task<IAvatarFile?> GetAsync(AvatarId avatarId, CancellationToken cancellationToken);
+        Task SetAsync(AvatarId avatarId, IAvatarFile avatarFile, CancellationToken cancellationToken);
+    }
+}
